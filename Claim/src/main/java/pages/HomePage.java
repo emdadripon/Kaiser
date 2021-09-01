6ac26b25256a2db00b9acb51b8756c70c03d6636
emdadripon <eriponctg@gmail.com>
2021-09-01 11:23:07 -0400

    initial commit

---
+++ Claim/src/main/java/pages/HomePage.java
@@ -0,0 +1,6 @@
+package pages;
+
+public class HomePage {
+
+    //https://choiceproducts-midatlantic.kaiserpermanente.org/flexible-choice/member-information/claims/
+}
---
+++ Finance/src/main/java/pages/HomePage.java
@@ -0,0 +1,5 @@
+package pages;
+
+public class HomePage {
+    //https://about.kaiserpermanente.org/community-health/communities-we-serve/mid-atlantic-community/medical-financial-assistance-program
+}
